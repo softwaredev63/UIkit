60f1eeec6c4a9554b18dbe9d2269f88ba76a0345
Alexey Shalin <shalin.2001@bk.ru>
2021-08-27 09:02:45 +0700

    Remove dist folder and ignore it

--- dist/widgets/Menu/PanelBodyHorizontal.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { PanelProps, PushedProps } from "./types";
-interface Props extends PanelProps, PushedProps {
-    isMobile: boolean;
-}
-declare const PanelBodyHorizontal: React.FC<Props>;
-export default PanelBodyHorizontal;
