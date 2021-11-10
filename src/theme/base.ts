6296ed5a8807b188576bcb296b3e0a90776d3dc3
SuperWebMan <aleksandrshreider@outlook.com>
2021-11-11 01:56:12 +0900

    Change screen size

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -1,7 +1,7 @@
 import { MediaQueries, Breakpoints, Spacing } from "./types";
 
 export const breakpointMap: { [key: string]: number } = {
-  xs: 370,
+  xs: 350,
   sm: 576,
   md: 852,
   lg: 968,
